212ec16fe59432dfcacf4f07174f00480ec0ed6b
Rolderek <restaslaci@gmail.com>
2021-04-29 10:38:25 +0200

    adddigits kész

--- src/main/java/addigits/AddDigits.java
+++ src/main/java/addigits/AddDigits.java
@@ -1,4 +1,23 @@
 package addigits;
 
 public class AddDigits {
+
+    public int addDigits(String input) {
+        int result = 0;
+        String num = null;
+        if (input == null || input.length() == 0) {
+            result = -1;
+        } else {
+             if (input.length() > 0) {
+                for (Character c : input.toCharArray()) {
+                    if (Character.isDigit(c)) {
+                        num = c.toString();
+                        result = result + Integer.parseInt(num);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 }
